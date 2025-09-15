cda580707189be4a70785799370b2a67831d4ea7
Grace Rosario <gracerosario398@gmail.com>
2025-09-15 21:09:20 +0530

    product page made responive

--- script.js
+++ script.js
@@ -12,30 +12,10 @@ function toggleMenu() {
     }
 
     function orderNow() {
-             function getLocation() {
-         if (navigator.geolocation) {
-           navigator.geolocation.getCurrentPosition(showPos, showErr);
-         } else {
-           document.getElementById("loc").innerText = "Geolocation not supported.";
-         }
-       }
-       
-       function showPos(pos) {
-         const lat = pos.coords.latitude;
-         const lng = pos.coords.longitude;
-         document.getElementById("loc").innerText = `Lat: ${lat}, Lng: ${lng}`;
-       
-         // you could even generate a Google Maps link
-         console.log(`https://maps.google.com/?q=${lat},${lng}`);
-       }
-       
-       function showErr(err) {
-         console.log(err);
-       }
       let dishName = document.getElementById("name").innerText;
       let qty = document.getElementById("quantity").value;
       let phone = "919137909697"; // replace with restaurant number
-      let msg = `Hi, I want to order ${dishName}.\nQuantity: ${qty}\nDelivery Address: My location: https://www.google.com/maps?q=${lat},${lon}\nPayment: (COD/UPI)\nContact number: (optional)`;
+      let msg = `Hi, I want to order ${dishName}.\nQuantity: ${qty}\nDelivery Address: (enter address here)\nPayment: (COD/UPI)\nContact number: (optional)`;
       
       let url = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
       window.open(url, "_blank");
