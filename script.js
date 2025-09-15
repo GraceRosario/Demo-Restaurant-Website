a069f3dfcc597964557000b717a0f6c42ab470ee
Grace Rosario <gracerosario398@gmail.com>
2025-09-15 20:11:13 +0530

    quantity added and btn changed

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function toggleMenu() {
         dropdown.style.display = "none";
       }
     }
+
+    function orderNow() {
+      let dishName = document.getElementById("name").innerText;
+      let qty = document.getElementById("quantity").value;
+      let phone = "919137909697"; // replace with restaurant number
+      let msg = `Hi, I want to order ${dishName}.\nQuantity: ${qty}\nDelivery Address: (enter address here)\nPayment: (COD/UPI)\nContact number: (optional)`;
+      
+      let url = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
+      window.open(url, "_blank");
+    }
