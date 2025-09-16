e1a1bdd2e3b09c563f45e35aa7b2145fa134ba40
Grace Rosario <gracerosario398@gmail.com>
2025-09-16 13:35:20 +0530

    non veg main Course completed

--- script.js
+++ script.js
@@ -32,3 +32,30 @@ function toggleMenu() {
       const url = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
       window.open(url, "_blank");
     }
+
+    function orderPieces() {
+      let basePrice = document.getElementById("price").innerText;
+      basePrice = basePrice.replace(/[^0-9.]/g, ""); // removes ₹
+      basePrice = parseFloat(basePrice); // makes it a number (120)
+      const quantity = parseFloat(document.getElementById('quantity').value);
+      const totalPrice = quantity * basePrice;
+      const dishName = document.getElementById("name").innerText;
+      const qty = document.getElementById("quantity").value;
+      const phone = "919137909697"; // replace with restaurant number
+      const quantityText =  
+          quantity === 1 ? "1 piece" : 
+          quantity === 2 ? "2 pieces" : 
+          quantity === 3 ? "3 pieces" : 
+          quantity === 4 ? "4 pieces" :
+          quantity === 4 ? "5 pieces" :
+          quantity === 4 ? "6 pieces" :
+          quantity === 4 ? "7 pieces" :
+          quantity === 4 ? "8 pieces" :
+          quantity === 4 ? "9 pieces" :
+          quantity === 4 ? "10 pieces" : "";
+
+      const msg = `Hi, I want to order ${dishName}.\nQuantity: ${qty}\nDelivery Address: (enter address here)\nPayment: (COD/UPI)\nContact number: (optional)\n${quantityText} product(s).\nTotal price: ₹${totalPrice}.`;
+      
+      const url = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
+      window.open(url, "_blank");
+    }
